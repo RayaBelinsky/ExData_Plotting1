5c683aa72c370ab98f7c39e7282717232b6542d3
RayaBelinsky <raiskab@gmail.com>
2015-05-11 01:19:24 +0300

    Update load_data.R

--- load_data.R
+++ load_data.R
@@ -2,15 +2,15 @@
 ##
 
 ## This file is for loading the large dataset.
-filename <- "C:/Users/rayab/Desktop/household_power_consumption.txt"
-data <- read.table(filename,
+filename <- "C:/Users/1/Documents/RProjects/exdata-data-household_power_consumption/household_power_consumption.txt"
+data1 <- read.table(filename,
                    header=TRUE,sep=";",
                    colClasses=c("character", "character", rep("numeric",7)),na="?")
-dim(data) # 2075259 9
-attach(data)
+dim(data1) # 2075259 9
+attach(data1)
 
 subset <- Date=="1/2/2007" | Date=="2/2/2007"
-newData <- data[subset, ]
+newData <- data1[subset, ]
 attach(newData)
 x <- paste(Date, Time)
 newData$DateTime <- strptime(x, "%d/%m/%Y %H:%M:%S")
